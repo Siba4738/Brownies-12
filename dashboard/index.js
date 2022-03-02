ac0e48ea346f90cab126f81894f9a0c0c5dfd056
Siba4738 <64182228+Siba4738@users.noreply.github.com>
2022-03-02 14:27:03 +0530

    Update index.js

--- dashboard/index.js
+++ dashboard/index.js
@@ -325,7 +325,7 @@ module.exports = client => {
      */
     //START THE WEBSITE ON THE DEFAULT PORT (80)
     const http = require(`http`).createServer(app);
-    http.listen(settings.config.http.port, () => {
+    http.listen(8080, () => {
         console.log(`[${settings.website.domain}]: HTTP-Website running on ${settings.config.http.port} port.`)
     });
 }
